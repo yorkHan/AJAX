8ebaf7d230e964b57f8abd6626ec569d319d0498
yorkHan <393900482@qq.com>
2018-04-22 23:15:22 +0800

    自己实现AJAX，并加上promise解决回调问题

--- main.js
+++ main.js
@@ -1,15 +1,49 @@
+window.jQuery=function(nodeOrSelector){
+    var nodes={
+        0:nodeOrSelector,
+        length:1
+    }
+    nodes.addClass=function () {
+    }
+    nodes.getSiblings=function () {
+    }
+    return noodes
+}
+window.$=window.jQuery
+
 oBtn.addEventListener('click',function () {
-    var request=new XMLHttpRequest()
-    request.open('get','/xxx')
-    request.send()
+    window.jQuery.ajax({
+    url:'/xxx',
+    method:'post',
+    type:{
+        'content-type':'application/x-www-form-urlencoded',
+        'frank': '18'
+      },
+    body:'我就是想改请求的第四部分'
+    }).then(function (text) {
+        console.log(text)
+    },function (request) {
+        console.log(request)
+    })
+})
+
+window.jQuery.ajax=function({url,method,type,body,success,fail}){
+    return new Promise(function (resolve,reject) {
+        var request=new XMLHttpRequest()
+    request.open(method,url)
+    for(let key in type){
+        let value = type[key]
+        request.setRequestHeader(key, value)
+    }
     request.onreadystatechange=function () {
         if(request.readyState===4){
             if(request.status>=200&&request.status<300){
-                let string=request.responseText
-                let obj=window.JSON.parse(string)
-                console.log(obj.note.name)
-                console.log(obj.note.age)
+                resolve.call(undefined,request.responseText)
+            }else if(request.status>400){
+                reject.call(undefined,request)
             }
         }
     }
-})
+    request.send(body)
+    })
+}
